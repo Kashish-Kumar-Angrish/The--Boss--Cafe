fc3b981720b0e3289808ab4210b2aa521f7fd2de
Kashish-Kumar-Angrish <kashishkumarverma265@gmail.com>
2025-01-24 17:03:40 +0530

    added imgs

---
+++ Script.js
@@ -0,0 +1,22 @@
+let button = document.querySelector("#Explore");
+console.log(button);
+
+button.onclick = function redirectToImage() {
+    // Array of image URLs
+    var imageUrls = [
+        'img/menupage3.png.jpg',
+        'img/menupage2.png.jpg',
+        'img/menupage1.png.jpg',
+    ];
+
+    // Get a random index
+    var randomIndex = Math.floor(Math.random() * imageUrls.length);
+
+    // Redirect to the randomly selected image
+    window.location.href = imageUrls[randomIndex];
+};
+
+let contact = document.querySelector("#contact-btn");
+contact.onclick = function directtoimg() {
+    window.location.href = "img/WhatsApp number.jpg";
+}
